050e5c92d3e33e5ad7f0a4a3eb66c174236460ba
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-04 22:36:06 +0700

    bai1. Khai Báo và Tính toán

---
+++ Baitap1_BienPhepToan/src/BaiTap1.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class BaiTap1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// 1. Khai báo các biến
+        int a = 15;
+        int b = 4;
+        double c = 3.5;
+
+        // 2. Tính toán
+        int tong = a + b;
+        double thuong = a / c;
+        int phanDu = a % b;
+
+        // 3. In kết quả
+        System.out.println("Tổng: " + tong);
+        System.out.println("Thương: " + thuong);
+        System.out.println("Phần dư: " + phanDu);
+
+	}
+
+}
