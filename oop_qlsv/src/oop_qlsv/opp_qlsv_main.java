06192243f5ba2c53ec8fb0f5b9504d1b8952d4c5
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-05 00:22:18 +0700

    Create opp_qlsv_main.java

---
+++ oop_qlsv/src/oop_qlsv/opp_qlsv_main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package oop_qlsv;
+
+/**
+ * 
+ */
+public class opp_qlsv_main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
