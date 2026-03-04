b5e89be072f44ba682fe2d92265905f5f30abb1e
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-05 00:12:30 +0700

    bài 3
    hello wordl

---
+++ Bai3_TinhToan/src/TinhToanDonGian.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class TinhToanDonGian {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10;
+        int b = 5;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+
+        System.out.println("Cộng: " + (a + b));
+        System.out.println("Trừ: " + (a - b));
+        System.out.println("Nhân: " + (a * b));
+        System.out.println("Chia: " + (a / b));
+
+	}
+
+}
