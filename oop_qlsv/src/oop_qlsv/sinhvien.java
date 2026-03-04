2d852d468122453f5484e77f352928ceb9dfa577
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-05 00:20:15 +0700

    ..

---
+++ oop_qlsv/src/oop_qlsv/sinhvien.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package oop_qlsv;
+
+/**
+ * 
+ */
+public class sinhvien {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
