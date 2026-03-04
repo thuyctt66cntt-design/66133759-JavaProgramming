791a3e7ec75e1a7339b156c0f977bbaa81b2a5c1
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-05 00:14:49 +0700

    run bài 4
    hello world

---
+++ Bai4_HelloWorld/src/HelloWorld.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String greeting = """
+	            ======================
+	            HELLO, MODERN JAVA!
+	            ======================
+	            by Chu Thị Thanh Thúy NTU
+	            ======================
+	            """;
+
+	        System.out.println(greeting);
+
+	}
+
+}
