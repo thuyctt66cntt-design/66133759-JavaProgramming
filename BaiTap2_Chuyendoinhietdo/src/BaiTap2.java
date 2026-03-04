6322b18d62be05ec6ec387d14ea5118b4a3b47ab
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-04 22:56:01 +0700

    bai2. chuyển đổi nhiệt

---
+++ BaiTap2_Chuyendoinhietdo/src/BaiTap2.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+public class BaiTap2 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double celsius = 25.0;        
+        double fahrenheit = (celsius * 9.0 / 5) + 32;  
+        System.out.println(celsius + "°C = " + fahrenheit + "°F");
+	}
+
+}
