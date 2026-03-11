915d98ca1c5a76c8e369f4953ac55b7153c2f17b
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-11 20:02:25 +0700

    1.3
    upcode

--- LopHinhChuNhat_Ex_Rectangle_1_3/src/Rectangle.java
+++ LopHinhChuNhat_Ex_Rectangle_1_3/src/Rectangle.java
@@ -1,4 +1,33 @@
 
 public class Rectangle {
+	private float length = 1.0f;
+    private float width = 1.0f;
+    public Rectangle() {
+    }
+    public Rectangle(float length, float width) {
+        this.length = length;
+        this.width = width;
+    }
+    public float getLength() {
+        return length;
+    }
+    public void setLength(float length) {
+        this.length = length;
+    }
+    public float getWidth() {
+        return width;
+    }
+    public void setWidth(float width) {
+        this.width = width;
+    }
+    public double getArea() {
+        return length * width;
+    }
+    public double getPerimeter() {
+        return 2 * (length + width);
+    }
+    public String toString() {
+        return "Hình chữ nhật[chiều dài=" + length + ", chiều rộng=" + width + "]";
+    }
 
 }
--- LopHinhChuNhat_Ex_Rectangle_1_3/src/Testmain.java
+++ LopHinhChuNhat_Ex_Rectangle_1_3/src/Testmain.java
@@ -3,6 +3,17 @@ public class Testmain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Rectangle r1 = new Rectangle(1.2f, 3.4f);
+        System.out.println(r1);
+        Rectangle r2 = new Rectangle();
+        System.out.println(r2);
+        r1.setLength(5.6f);
+        r1.setWidth(7.8f);
+        System.out.println(r1);
+        System.out.println("Chiều dài: " + r1.getLength());
+        System.out.println("Chiều rộng: " + r1.getWidth());
+        System.out.printf("Diện tích: %.2f\n", r1.getArea());
+        System.out.printf("Chu vi: %.2f\n", r1.getPerimeter());
 
 	}
 
