1569cf5afc2a907c3f8cb7bedc4a801b24f40ee0
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-11 20:43:38 +0700

    1.5
    upcode

--- LopInvoiceItem_Ex_InvoiceItem_1_5/src/TestMain.java
+++ LopInvoiceItem_Ex_InvoiceItem_1_5/src/TestMain.java
@@ -3,6 +3,16 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		InvoiceItem inv1 = new InvoiceItem("A101", "Bút đỏ", 888, 0.08);
+        System.out.println(inv1);
+        inv1.setQty(999);
+        inv1.setUnitPrice(0.99);
+        System.out.println(inv1);
+        System.out.println("id là: " + inv1.getId());
+        System.out.println("mô tả là: " + inv1.getDesc());
+        System.out.println("số lượng là: " + inv1.getQty());
+        System.out.println("giá đơn vị là: " + inv1.getUnitPrice());
+        System.out.println("Tổng cộng là: " + inv1.getTotal());
 
 	}
 
