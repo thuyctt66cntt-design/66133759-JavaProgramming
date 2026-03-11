cc395d7f69ba5a1a7d746a3e43684ea717b93c96
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-11 19:29:56 +0700

    1.2

--- LopHinhTron_Ex_Circle_1_2/src/Circle.java
+++ LopHinhTron_Ex_Circle_1_2/src/Circle.java
@@ -1,4 +1,24 @@
 
 public class Circle {
-
+	private double radius = 1.0;
+    public Circle() {
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+    public double getCircumference() {
+        return 2 * Math.PI * radius;
+    }
+    public String toString() {
+        return "Hình tròn[bán kính=" + radius + "]";
+    }
 }
