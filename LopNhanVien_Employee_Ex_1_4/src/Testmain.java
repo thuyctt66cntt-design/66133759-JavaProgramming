a0729638d8b28b051150fc4b7b78b0fe172e2604
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-11 20:31:42 +0700

    1.4
    upcode

--- LopNhanVien_Employee_Ex_1_4/src/Testmain.java
+++ LopNhanVien_Employee_Ex_1_4/src/Testmain.java
@@ -3,7 +3,18 @@ public class Testmain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Employee e1 = new Employee(8, "Peter", "Tan", 2500);
+        System.out.println(e1);
+        e1.setSalary(999);
+        System.out.println(e1);
+        System.out.println("id là: " + e1.getId());
+        System.out.println("tên là: " + e1.getFirstName());
+        System.out.println("họ là: " + e1.getLastName());
+        System.out.println("lương là: " + e1.getSalary());
+        System.out.println("tên là: " + e1.getName());
+        System.out.println("lương hàng năm là: " + e1.getAnnualSalary());
+        System.out.println(e1.raiseSalary(10));
+        System.out.println(e1);
 	}
 
 }
