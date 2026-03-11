6a2cff2630d5a5f79051b36a53cfa7fabf28f893
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-11 18:26:45 +0700

    update

--- LopHinhTron_Ex_Circle_1_1/src/Circle.java
+++ LopHinhTron_Ex_Circle_1_1/src/Circle.java
@@ -1,24 +1,36 @@
 
 public class Circle {
-	  private double radius;
-	    private String color;
-
-	    public Circle() {
-	        radius = 1.0;
-	        color = "red";
-	    }
-
-	    public Circle(double r) {
-	        radius = r;
-	        color = "red";
-	    }
-
-	    public double getRadius() {
-	        return radius;
-	    }
-
-	    public double getArea() {
-	        return radius * radius * Math.PI;
-	    }
+	private double radius;
+    private String color;
+    public Circle() {
+        this.radius = 1.0;
+        this.color = "red";
+    }
+    public Circle(double radius) {
+        this.radius = radius;
+        this.color = "red";
+    }
+    public Circle(double radius, String color) {
+        this.radius = radius;
+        this.color = color;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public String getColor() {
+        return color;
+    }
+    public double getArea() {
+        return radius * radius * Math.PI;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+    public void setColor(String color) {
+        this.color = color;
+    }
+    public String toString() {
+        return "Circle[radius=" + radius + " color=" + color + "]";
+    }
 	}
 
--- LopHinhTron_Ex_Circle_1_1/src/TestCircle.java
+++ LopHinhTron_Ex_Circle_1_1/src/TestCircle.java
@@ -2,18 +2,25 @@
 public class TestCircle{
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	      Circle c1 = new Circle();
-
-	      System.out.println("Hình tròn có bán kính là: " 
-	         + c1.getRadius() + "\nVà diện tích là: " + c1.getArea());
-
-	      Circle c2 = new Circle(2.0);
-
-	      System.out.println("Hình tròn bán kính là: " 
-	         + c2.getRadius() + " \nVà diện tích là: " + c2.getArea());
-
-	}
+        Circle c1 = new Circle();
+        System.out.println( "Hình tròn có bán kính là:"
+                + c1.getRadius() + " \nVà diện tích là: " + c1.getArea());
+        Circle c2 = new Circle(2.0);
+        System.out.println("Hình tròn có bán kính là "
+                + c2.getRadius() + "\nVà diện tích là: " + c2.getArea());
+        Circle c3 = new Circle(3.0, "blue");
+        System.out.println("Màu: " + c3.getColor());
+        Circle c4 = new Circle();
+        c4.setRadius(5.5);
+        System.out.println("ban kinh la: " + c4.getRadius());
+        c4.setColor("green");
+        System.out.println("mau la: " + c4.getColor());
+        Circle c5 = new Circle(5.5);
+        System.out.println(c5.toString());
+        Circle c6 = new Circle(6.6);
+        System.out.println(c6.toString());
+        System.out.println(c6);
+        System.out.println("Toan tu + goi toString(): " + c6);
+    }
 
 }
