323c7008b65e95029c15fe8e2a420649afd95b27
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-18 20:23:43 +0700

    bài 1 Phần B

--- PHANB_BTTH_ThanhThuy/src/tahty_26/SanPham.java
+++ PHANB_BTTH_ThanhThuy/src/tahty_26/SanPham.java
@@ -1,5 +1,80 @@
 package tahty_26;
 
 public class SanPham {
+	private String maSP;
+    private String tenSP;
+    private double gia;
+    private int soLuong;
+
+    // Constructor mặc định
+    public SanPham() {
+    }
+
+    // Constructor có tham số
+    public SanPham(String maSP, String tenSP, double gia, int soLuong) {
+        setMaSP(maSP);
+        setTenSP(tenSP);
+        setGia(gia);
+        setSoLuong(soLuong);
+    }
+
+    // Getter & Setter
+    public String getMaSP() {
+        return maSP;
+    }
+
+    public void setMaSP(String maSP) {
+        if (maSP != null && !maSP.trim().isEmpty()) {
+            this.maSP = maSP;
+        } else {
+            System.out.println("Mã sản phẩm không được rỗng!");
+        }
+    }
+
+    public String getTenSP() {
+        return tenSP;
+    }
+
+    public void setTenSP(String tenSP) {
+        this.tenSP = tenSP;
+    }
+
+    public double getGia() {
+        return gia;
+    }
+
+    public void setGia(double gia) {
+        if (gia > 0) {
+            this.gia = gia;
+        } else {
+            System.out.println("Giá phải > 0!");
+        }
+    }
+
+    public int getSoLuong() {
+        return soLuong;
+    }
+
+    public void setSoLuong(int soLuong) {
+        if (soLuong >= 0) {
+            this.soLuong = soLuong;
+        } else {
+            System.out.println("Số lượng phải ≥ 0!");
+        }
+    }
+
+    // Tính thành tiền
+    public double tinhThanhTien() {
+        return gia * soLuong;
+    }
+
+    // Hiển thị thông tin
+    public void hienThiThongTin() {
+        System.out.println("Mã SP: " + maSP);
+        System.out.println("Tên SP: " + tenSP);
+        System.out.println("Giá: " + gia);
+        System.out.println("Số lượng: " + soLuong);
+        System.out.println("Thành tiền: " + tinhThanhTien());
+    }
 
 }
--- PHANB_BTTH_ThanhThuy/src/tahty_26/TestMain.java
+++ PHANB_BTTH_ThanhThuy/src/tahty_26/TestMain.java
@@ -4,6 +4,15 @@ public class TestMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Tạo sản phẩm hợp lệ
+        SanPham sp1 = new SanPham("SP01", "Bánh", 10000, 5);
+        sp1.hienThiThongTin();
+
+        System.out.println("--------------------");
+
+        // Test dữ liệu sai (validation)
+        SanPham sp2 = new SanPham("", "Nước", -5000, -2);
+        sp2.hienThiThongTin();
 
 	}
 
