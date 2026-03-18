54e999d3a4398969b198a74c7a44b661b48ce2db
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-18 20:47:34 +0700

    bài 2 phần B
    upcode

--- PHANB_BAI2_ThanhThuy/src/tahty_26/NhanVien.java
+++ PHANB_BAI2_ThanhThuy/src/tahty_26/NhanVien.java
@@ -1,5 +1,78 @@
 package tahty_26;
 
 public class NhanVien {
+	private String maNV;
+    private String hoTen;
+    private double luongCoBan;
+    private double heSoLuong;
+
+    // Constructor có tham số
+    public NhanVien(String maNV, String hoTen, double luongCoBan, double heSoLuong) {
+        this.maNV = maNV;
+        this.hoTen = hoTen;
+        this.luongCoBan = luongCoBan;
+        this.heSoLuong = heSoLuong;
+    }
+
+    // Getter & Setter
+    public String getMaNV() {
+        return maNV;
+    }
+
+    public void setMaNV(String maNV) {
+        this.maNV = maNV;
+    }
+
+    public String getHoTen() {
+        return hoTen;
+    }
+
+    public void setHoTen(String hoTen) {
+        this.hoTen = hoTen;
+    }
+
+    public double getLuongCoBan() {
+        return luongCoBan;
+    }
+
+    public void setLuongCoBan(double luongCoBan) {
+        this.luongCoBan = luongCoBan;
+    }
+
+    public double getHeSoLuong() {
+        return heSoLuong;
+    }
+
+    public void setHeSoLuong(double heSoLuong) {
+        this.heSoLuong = heSoLuong;
+    }
+
+    // Tính lương
+    public double tinhLuong() {
+        return luongCoBan * heSoLuong;
+    }
+
+    // Xếp loại
+    public String xepLoai() {
+        double luong = tinhLuong();
+
+        if (luong >= 20000000) {
+            return "Xuất sắc";
+        } else if (luong >= 15000000) {
+            return "Tốt";
+        } else {
+            return "Khá";
+        }
+    }
+
+    // Hiển thị thông tin
+    public void hienThiThongTin() {
+        System.out.println("Mã NV: " + maNV);
+        System.out.println("Họ tên: " + hoTen);
+        System.out.println("Lương cơ bản: " + luongCoBan);
+        System.out.println("Hệ số lương: " + heSoLuong);
+        System.out.println("Lương: " + tinhLuong());
+        System.out.println("Xếp loại: " + xepLoai());
+    }
 
 }
--- PHANB_BAI2_ThanhThuy/src/tahty_26/testMain.java
+++ PHANB_BAI2_ThanhThuy/src/tahty_26/testMain.java
@@ -4,6 +4,18 @@ public class testMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		NhanVien nv1 = new NhanVien("NV01", "An", 5000000, 4);
+        nv1.hienThiThongTin();
+
+        System.out.println("--------------------");
+
+        NhanVien nv2 = new NhanVien("NV02", "Bình", 5000000, 3);
+        nv2.hienThiThongTin();
+
+        System.out.println("--------------------");
+
+        NhanVien nv3 = new NhanVien("NV03", "Cường", 4000000, 3);
+        nv3.hienThiThongTin();
 
 	}
 
