0d5362be898c1a805f7195cf7c462a4bd1a91d07
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-03-18 22:14:54 +0700

    Bài 3 Phần B
    up code

--- BAI3_PHANB_ThanhThuy/src/tahty_26/Diem.java
+++ BAI3_PHANB_ThanhThuy/src/tahty_26/Diem.java
@@ -1,5 +1,57 @@
 package tahty_26;
 
 public class Diem {
+	 private double x;
+	    private double y;
 
-}
+	    // Constructor mặc định (0,0)
+	    public Diem() {
+	        this.x = 0;
+	        this.y = 0;
+	    }
+
+	    // Constructor có tham số
+	    public Diem(double x, double y) {
+	        this.x = x;
+	        this.y = y;
+	    }
+
+	    // Getter & Setter
+	    public double getX() {
+	        return x;
+	    }
+
+	    public void setX(double x) {
+	        this.x = x;
+	    }
+
+	    public double getY() {
+	        return y;
+	    }
+
+	    public void setY(double y) {
+	        this.y = y;
+	    }
+
+	    // Tính khoảng cách giữa 2 điểm
+	    public double tinhKhoangCach(Diem d) {
+	        return Math.sqrt(Math.pow(this.x - d.x, 2) +
+	                         Math.pow(this.y - d.y, 2));
+	    }
+
+	    // Kiểm tra trùng nhau
+	    public boolean kiemTraTrungNhau(Diem d) {
+	        return this.x == d.x && this.y == d.y;
+	    }
+
+	    // Đối xứng qua gốc tọa độ
+	    public Diem doiXungQuaGoc() {
+	        return new Diem(-this.x, -this.y);
+	    }
+
+	    // Hiển thị
+	    public void hienThi() {
+	        System.out.println("(" + x + ", " + y + ")");
+	    }
+	}
+
--- BAI3_PHANB_ThanhThuy/src/tahty_26/testMain.java
+++ BAI3_PHANB_ThanhThuy/src/tahty_26/testMain.java
@@ -4,6 +4,30 @@ public class testMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 Diem d1 = new Diem(2, 3);
+	        Diem d2 = new Diem(5, 7);
+
+	        // Hiển thị điểm
+	        System.out.print("Điểm d1: ");
+	        d1.hienThi();
+
+	        System.out.print("Điểm d2: ");
+	        d2.hienThi();
+
+	        // Tính khoảng cách
+	        System.out.println("Khoảng cách: " + d1.tinhKhoangCach(d2));
+
+	        // Kiểm tra trùng nhau
+	        if (d1.kiemTraTrungNhau(d2)) {
+	            System.out.println("Hai điểm trùng nhau");
+	        } else {
+	            System.out.println("Hai điểm không trùng nhau");
+	        }
+
+	        // Điểm đối xứng
+	        Diem dx = d1.doiXungQuaGoc();
+	        System.out.print("Điểm đối xứng của d1: ");
+	        dx.hienThi();
 
 	}
 
