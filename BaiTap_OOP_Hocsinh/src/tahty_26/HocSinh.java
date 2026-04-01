47d6f38b5a9b7dc514db4158c08830329738f081
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-04-01 23:30:00 +0700

    ..

--- BaiTap_OOP_Hocsinh/src/tahty_26/HocSinh.java
+++ BaiTap_OOP_Hocsinh/src/tahty_26/HocSinh.java
@@ -1,5 +1,48 @@
 package tahty_26;
 
 public class HocSinh {
+	private String tenHS, lopHS;
+	private short tuoiHS;
+	public HocSinh() 
+	{
+		
+	}
+	public HocSinh(String tenHS, String lopHS, short tuoiHS)
+	{
+		super();
+		this.tenHS = tenHS;
+		this.lopHS = lopHS;
+		this.tuoiHS = tuoiHS;
+	}
+	public String getTenHS() 
+	{
+		return this.tenHS;
+	}
+	public String getLopHS()
+	{
+		return this.lopHS;
+	}
+	public short getTuoiHS() 
+	{
+		return this.tuoiHS;
+	}
+	public void setTenHS(String tenHS)
+	{
+		this.tenHS = tenHS;
+	}
+	public void setTuoiHS(short tuoiHS)
+	{
+		this.tuoiHS = tuoiHS;
+	}
+	public void setLopHS(String lopHS)
+	{
+		this.lopHS = lopHS;
+	}
+	@Override
+	public String toString()
+	{
+		return ("HS " + tenHS + " " + tuoiHS + " tuoi" + " dang hoc lop " + lopHS);
+		
+	} 
 
 }
--- BaiTap_OOP_Hocsinh/src/tahty_26/Main.java
+++ BaiTap_OOP_Hocsinh/src/tahty_26/Main.java
@@ -1,4 +1,6 @@
 package tahty_26;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
