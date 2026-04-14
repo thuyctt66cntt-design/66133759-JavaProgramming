6ab9a17563faa0ccd481c3ae69bb28085f0a0fe1
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-04-14 20:51:09 +0700

    Update ViDuDocStream.java

--- VidustreamDocChuoi/src/tahty_26/ViDuDocStream.java
+++ VidustreamDocChuoi/src/tahty_26/ViDuDocStream.java
@@ -6,14 +6,14 @@ import java.io.InputStreamReader;
 
 public class ViDuDocStream {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		//Khai báo biến đối tượng InputStreamRedear
-		InputStreamReader ipReader = new InputStreamReader(system.in);
+		InputStreamReader ipReader = new InputStreamReader(System.in);
 		//Khai báo biến đối tượng Buffee
 		BufferedReader bufReader = new BufferedReader(ipReader);
 		// In ra 1 câu hướng dẫn để User biết câng làm gì
-		system.out.print("Nhập 1 chuỗi bất kì: ");
+		System.out.print("Nhập 1 chuỗi bất kì: ");
 		//Đọc dữ liệu từ bàn phím, cất vào biến chuỗi
 		String lineDocDuoc = bufReader.readLine();
 		//In ra dòng đọc được
