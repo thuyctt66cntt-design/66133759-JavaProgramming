cdcb9f6e0d835a344b2851c39b71e91b1052c04d
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-04-16 00:03:18 +0700

    ví dụ PrintWriter

---
+++ viduPrintWriter/src/tahty_26/PrintWriterDemo.java
@@ -0,0 +1,18 @@
+package tahty_26;
+import java.io.*;
+public class PrintWriterDemo {
+
+	public static void main(String[] args) {
+		PrintWriter pw = new PrintWriter(System.out, true);
+		int i=10;
+		double d=123.67;
+		double r= i+d;
+		
+		pw.println("Using a PrintWriter.");
+		pw.println(i);
+		pw.println(d);
+		pw.println( i + "+" + d + "="+ r);
+
+	}
+
+}
