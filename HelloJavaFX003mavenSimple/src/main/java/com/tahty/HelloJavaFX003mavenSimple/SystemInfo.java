bba2b5685c7e69163fed22ccde69e19deba92062
thuyctt66cntt-design <thuy.ctt.66cntt@ntu.edu.vn>
2026-05-08 20:39:15 +0700

    HelloJavaFX003

---
+++ HelloJavaFX003mavenSimple/src/main/java/com/tahty/HelloJavaFX003mavenSimple/SystemInfo.java
@@ -0,0 +1,13 @@
+package com.tahty.HelloJavaFX003mavenSimple;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ HelloJavaFX003mavenSimple/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tahty.HelloJavaFX003mavenSimple {
+    requires javafx.controls;
+    exports com.tahty.HelloJavaFX003mavenSimple;
+}
